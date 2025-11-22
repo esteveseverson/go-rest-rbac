e5eb5d905263739426db09d2eac81025f0e2fb34
Everson <esteveseverson@gmail.com>
2025-11-22 11:21:24 -0300

    feat(auth): create response, password hashing and jwt modules

--- internal/handlers/core_handler.go
+++ internal/handlers/core_handler.go
@@ -1,7 +1,19 @@
 package handlers
 
-type Handler struct{}
+import (
+	"database/sql"
 
-func NewHandler() *Handler {
-	return &Handler{}
+	"github.com/esteveseverson/go-rest-rbac/internal/store"
+)
+
+type Handler struct {
+	DB      *sql.DB
+	Queries *store.Queries
+}
+
+func NewHandler(db *sql.DB, queries *store.Queries) *Handler {
+	return &Handler{
+		DB:      db,
+		Queries: queries,
+	}
 }
---
+++ internal/utils/jwt.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Claims struct {
+	UserID   int64  `json:"user_id"`
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(userdId int64, username string, secretKey []byte) (string, error) {
+	claims := Claims{
+		UserID:   userdId,
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),
+			Issuer:    "abayomi",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
